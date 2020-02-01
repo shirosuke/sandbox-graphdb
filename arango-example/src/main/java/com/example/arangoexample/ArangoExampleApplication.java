af35542d4274c829512743ea2f610d4d612b02a0
shirosuke <tuketarou@gmail.com>
2020-02-01 12:21:17 +0900

    arangodbの雛形配置

---
+++ arango-example/src/main/java/com/example/arangoexample/ArangoExampleApplication.java
@@ -0,0 +1,11 @@
+package com.example.arangoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArangoExampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArangoExampleApplication.class, args);
+	}
+}
